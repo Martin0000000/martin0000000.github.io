1537885eb4ec9f15a48892ed863c88e677a0d505
Martin0000000 <martinbrown8@gmail.com>
2020-12-03 19:09:09 +0000

    Image size needed inverted commas

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 
 function myFunction() {
-   var element = document.body;
-   element.classList.toggle("dark-mode");
+  var element = document.body;
+  element.classList.toggle("dark-mode");
 }
-  document.addEventListener('DOMContentLoaded', function(){
-    document.getElementById('getMessage').onclick = function(){
-document.getElementsByClassName('message')[0].textContent= "Let Martin build your websites and apps. Don't use a robot!";
-    };
-  });
-
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('getMessage').onclick = function () {
+    document.getElementsByClassName('message')[0].textContent = "Let Martin build your websites and apps. Don't use a robot!";
+  };
+});
