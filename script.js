f122a782bbb79c537c8af7b00bd8b55d28cd8d34
Martin0000000 <martinbrown8@gmail.com>
2020-12-03 16:54:58 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function myFunction() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+}
+  document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('getMessage').onclick = function(){
+document.getElementsByClassName('message')[0].textContent= "Let Martin build your websites and apps. Don't use a robot!";
+    };
+  });
+
