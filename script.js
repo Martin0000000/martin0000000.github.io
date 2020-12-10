e5d047542114f1cf203be54760b607c87796f011
Martin0000000 <martinbrown8@gmail.com>
2020-12-10 12:43:22 +0000

    Added work history and slideshow

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
-
 function myFunction() {
-  var element = document.body;
-  element.classList.toggle("dark-mode");
+   var element = document.body;
+   element.classList.toggle("dark-mode");
 }
+  document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('getMessage').onclick = function(){
+document.getElementsByClassName('message')[0].textContent= "Let Martin build your websites and apps. Don't use a robot!";
+    };
+  });
 
-document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('getMessage').onclick = function () {
-    document.getElementsByClassName('message')[0].textContent = "Let Martin build your websites and apps. Don't use a robot!";
-  };
-});
+var slideshows = document.querySelectorAll('[data-component="slideshow"]');
+slideshows.forEach(initSlideShow);
+
+function initSlideShow(slideshow) {
+
+	var slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide`);
+
+	var index = 0, time = 4000;
+	slides[index].classList.add('active');
+
+	setInterval( () => {
+		slides[index].classList.remove('active');
+		
+		index++;
+		if (index === slides.length) index = 0;
+
+		slides[index].classList.add('active');
+
+	}, time);
+}
